614b9e971f789e6bcf2710f6bf5cfa459ee5febc
Szeri <szeri323@gmail.com>
2024-11-13 14:32:59 +0100

    add imports and update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
 from dotenv import load_dotenv
 from openai import OpenAI
-import os
+from gpts_roles import create_gpts
+from prompt import create_prompt
+from content_manager import read_from_file,write_to_file
 
 load_dotenv()
 
@@ -8,15 +11,39 @@ api_key = os.getenv('API_KEY')
 
 client = OpenAI(api_key=api_key)
 
+content = read_from_file()
+
+gpts = create_gpts()
+prompt = create_prompt(content)
+
 
 completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
+        gpts,
+        prompt
     ]
 )
 
-print(completion.choices[0].message)
+write_to_file(completion.choices[0].message,"", "")
+
+
+
+
+
+
+
+
+
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {
+#             "role": "user",
+#             "content": "Write a haiku about recursion in programming."
+#         }
+#     ]
+# )
+
+# print(completion.choices[0].message)    
