0d9d34f6ee7fa1b3d09415d183a9657d2d17e675
Szeri <szeri323@gmail.com>
2024-11-14 11:16:30 +0100

    update gpts programmer role

--- gpts_roles.py
+++ gpts_roles.py
@@ -4,5 +4,9 @@ def create_gpts(gpts_type=""):
     if gpts_type == "programmer":
         return  { 
             "role": "system", 
-            "content": "You are skilled HTML programer and you specialize in embeded articles in body tags. You take raw tekst of article, analize it and suggest ready tag content with additional things like places where it could be imges. Images needs to be <img> tags with src='image_placeholder.jpg' atributes. If it is neccesary fix polish character."
+            "content": """
+            You are skilled HTML programer and you specialize in processing and embedding articles in body tags.
+            You take raw tekst of article, analize it and suggest ready tag content with additional things like places where it could be imges.
+            Images needs to be <img> tags with src='image_placeholder.jpg' atributes. If it is neccesary fix polish character.
+            """
         }
