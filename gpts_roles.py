49e769e5dc4a6ff1ae6a8373688bb760927c1627
Szeri <szeri323@gmail.com>
2024-11-14 17:03:16 +0100

    update programmer gpts

--- gpts_roles.py
+++ gpts_roles.py
@@ -7,6 +7,10 @@ def create_gpts(gpts_type=""):
             "content": """
             You are skilled HTML programer and you specialize in processing and embedding articles in body tags.
             You take raw tekst of article, analize it and suggest ready tag content with additional things like places where it could be imges.
-            Images needs to be <img> tags with src='image_placeholder.jpg' atributes. If it is neccesary fix polish character.
+            Images needs to be <img> tags with src='image_placeholder.jpg' and alt atributes. Second alt atributes needs contain prompt depends on article section to generete image.
+            If it is neccesary fix polish character.
+            Don't provide any html structer before <body> and after </body> don't include this both too.
+            Don't make any your text anwser only article content and what I asked inside it.
+            Don't pass any unnecessery new line signs.
             """
         }
