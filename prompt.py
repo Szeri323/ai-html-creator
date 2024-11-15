db92102542050063fd79164ea2cc99161c1d1658
Szeri <szeri323@gmail.com>
2024-11-15 22:06:34 +0100

    update create prompt argument to be artibele by default

--- prompt.py
+++ prompt.py
@@ -1,5 +1,6 @@
-def create_prompt(content, prompt_type=""):
-    if prompt_type =="":
+def create_prompt(content, prompt_type="article"):
+    """ Returns prompt dictionary based on content and prompt_type. """
+    if prompt_type == "article":
         return  {
             "role": "user",
             "content": f"Chciałbym abyś użył tego tekstu: {content}."
